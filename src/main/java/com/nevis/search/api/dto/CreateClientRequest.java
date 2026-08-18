aaf6d6bc66071be951e3bb5b22222d4d80bdb560
Novov Andrei <novov.andrew@gmail.com>
2026-08-18 22:14:35 +0100

    Document required field lengths in OpenAPI

--- src/main/java/com/nevis/search/api/dto/CreateClientRequest.java
+++ src/main/java/com/nevis/search/api/dto/CreateClientRequest.java
@@ -2,16 +2,17 @@ package com.nevis.search.api.dto;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record CreateClientRequest(
         @JsonProperty("first_name") @JsonAlias("firstName")
-        @NotBlank @Size(max = 100) String firstName,
+        @NotBlank @Size(max = 100) @Schema(minLength = 1, maxLength = 100) String firstName,
         @JsonProperty("last_name") @JsonAlias("lastName")
-        @NotBlank @Size(max = 100) String lastName,
-        @NotBlank @Email @Size(max = 254) String email,
+        @NotBlank @Size(max = 100) @Schema(minLength = 1, maxLength = 100) String lastName,
+        @NotBlank @Email @Size(max = 254) @Schema(minLength = 1, maxLength = 254) String email,
         @Size(max = 100) String countryOfResidence
 ) {
 }
--- src/main/java/com/nevis/search/api/dto/CreateDocumentRequest.java
+++ src/main/java/com/nevis/search/api/dto/CreateDocumentRequest.java
@@ -2,10 +2,10 @@ package com.nevis.search.api.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 public record CreateDocumentRequest(
-        @NotBlank @Size(max = 255) String title,
+        @NotBlank @Size(max = 255) @Schema(minLength = 1, maxLength = 255) String title,
         @NotBlank String content
 ) {
 }
-
